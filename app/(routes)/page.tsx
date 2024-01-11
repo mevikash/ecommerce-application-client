b2177fc7bad581dca9b6fb18f5dad6e73f93572e
mevikash <badal8core@gmail.com>
2024-01-12 00:05:40 +0530

    Client 3

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -12,16 +12,15 @@ const HomePage = async () => {
 
   return (
     <Container>
-    //   <div className="space-y-10 pb-10">
-    //     <Billboard 
+      <div className="space-y-10 pb-10">
+        <Billboard 
           data={billboard}
         />
-    //     <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-    //       <ProductList title="Featured Products" items={products} />
-    //     </div>
-    //   </div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
     </Container>
-    // <div>Hello Store</div>
   )
 };
 
